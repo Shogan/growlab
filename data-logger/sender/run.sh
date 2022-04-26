ae8f4b1f2984edd2c7026df25a241a3ce4fd29cb
shogan <shogan85@gmail.com>
2022-04-26 18:53:18 +0100

    Update images at Tue 26 Apr 18:53:17 BST 2022
    Signed-off-by: shogan <shogan85@gmail.com>

--- data-logger/sender/run.sh
+++ data-logger/sender/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FUNCTION_URL="http://192.168.2.45:8080/function/submit-sample"
-SENSOR="bmp280"
+export FUNCTION_URL="http://192.168.2.45:8080/function/submit-sample"
+export SENSOR="bmp280"
+export SENSOR_TYPE="bmp280"
 python3 main.py
 
