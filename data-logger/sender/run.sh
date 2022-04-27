6b1842e43d3bee357098c43c67dfc96f5cb26872
shogan <shogan85@gmail.com>
2022-04-27 17:17:26 +0100

    Update images at Wed 27 Apr 17:17:26 BST 2022
    Signed-off-by: shogan <shogan85@gmail.com>

--- data-logger/sender/run.sh
+++ data-logger/sender/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export FUNCTION_URL="http://192.168.2.45:8080/function/submit-sample"
-export SENSOR="bmp280"
+export FUNCTION_URL="http://192.168.2.45:8080/function/submit-sample-to-influxdb"
+export SENSOR="growlab"
 export SENSOR_TYPE="bmp280"
 python3 main.py
 
