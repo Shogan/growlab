ed9a8b858cf71ded3353d34eacad9b2389b24d57
shogan <shogan85@gmail.com>
2022-04-27 18:18:28 +0100

    Update images at Wed 27 Apr 18:18:28 BST 2022
    Signed-off-by: shogan <shogan85@gmail.com>

--- data-logger/submit-sample/handler-jail.py
+++ data-logger/submit-sample/handler-jail.py
@@ -3,6 +3,25 @@ import os
 import time
 from datetime import datetime
 from influxdb import InfluxDBClient
+from sensors import growbme280, growbmp280
+
+sensor_name = os.getenv("SENSOR")
+sample_duration = 30  # seconds
+sensor = None
+sensor_type = os.getenv("SENSOR_TYPE", "bme280")
+print("Sensor type: {}", sensor_type)
+
+if sensor_type == "bme280":
+    sensor = growbme280()
+elif sensor_type == "bmp280":
+    sensor = growbmp280()
+
+def get_cpu_temp():
+    path="/sys/class/thermal/thermal_zone0/temp"
+    f = open(path, "r")
+    temp_raw = int(f.read().strip())
+    temp_cpu = float(temp_raw / 1000.0)
+    return temp_cpu
 
 def handle(req):
     """handle a request to the function
@@ -87,3 +106,35 @@ def make_points(r):
             })
 
     return points
+
+
+try:
+    while True:
+        print("Gathering sensor data.")
+        temp_cpu = get_cpu_temp()
+
+        readings = sensor.get_readings()
+
+        readings["sensor"] = sensor_name
+        readings["cpu_temperature"] = temp_cpu
+
+        my_date = datetime.now()
+        readings["iso_time"] = my_date.isoformat()
+        data = json.dumps(readings)
+        print(data)
+
+        try:
+            res = handle(function_url, data=data, headers={"Content-type": "application/json"})
+            if res.status_code != 200:
+                print("Unexpected status code: {}".format(res.status_code))
+            else:
+                print("Sent to function..OK.")
+
+        except Exception as e:
+            print(e)
+            continue
+        time.sleep(sample_duration)
+
+except KeyboardInterrupt:
+    pass
+
